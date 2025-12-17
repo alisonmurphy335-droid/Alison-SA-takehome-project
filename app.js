a079c8a8d63dd6b99ced3da8689a0321a2747ff5
alisonmurphy335-droid <alisonmurphy335@gmail.com>
2025-12-17 17:36:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -61,6 +61,21 @@ app.get('/checkout', function(req, res) {
   });
 });
 
+const options = {
+  clientSecret: '{{CLIENT_SECRET}}',
+  // Fully customizable with appearance API.
+  appearance: {/*...*/},
+};
+
+// Set up Stripe.js and Elements to use in checkout form, passing the client secret obtained in a previous step
+const elements = stripe.elements(options);
+
+// Create and mount the Payment Element
+const paymentElementOptions = { layout: 'accordion'};
+const paymentElement = elements.create('payment', paymentElementOptions);
+paymentElement.mount('#payment-element');
+
+
 /**
  * Success route
  */
